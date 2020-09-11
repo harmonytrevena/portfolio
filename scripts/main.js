5764c351173ad280c4bdc33de38695124f9c40b8
Harmony Trevena <harmonytrevena@gmail.com>
2020-09-10 21:10:17 -0400

    Updated HTML skeleton and started custom css.

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,30 @@
 // Wrap every letter in a span
-const textWrapper = document.querySelector('.ml11 .letters');
+const textWrapper = document.querySelector('.moving .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml11 .line',
+    targets: '.moving .line',
     scaleY: [0,1],
     opacity: [0.5,1],
     easing: "easeOutExpo",
     duration: 700
   })
   .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    targets: '.moving .line',
+    translateX: [0, document.querySelector('.moving .letters').getBoundingClientRect().width + 10],
     easing: "easeOutExpo",
     duration: 700,
     delay: 100
   }).add({
-    targets: '.ml11 .letter',
+    targets: '.moving .letter',
     opacity: [0,1],
     easing: "easeOutExpo",
     duration: 600,
     offset: '-=775',
     delay: (el, i) => 34 * (i+1)
   }).add({
-    targets: '.ml11',
+    targets: '.moving',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
