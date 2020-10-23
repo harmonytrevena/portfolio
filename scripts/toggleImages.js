cf5bbd2173ebfdbf9c9d8aceb313d4d29943e787
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-23 18:48:55 -0400

    Added all major dark mode graphics

--- scripts/toggleImages.js
+++ scripts/toggleImages.js
@@ -43,6 +43,14 @@ function toggleImages() {
         featureImg2.src = "./images/Featured_Project_Graphic.png";
     }
 
+    const featureGraphic = document.getElementById('featureGraphic');
+    if (featureGraphic.src.match("./images/Taskwork_Graphic.png")) {
+        featureGraphic.src = "./images/Dark_Taskwork_Graphic.png";
+    }
+    else {
+        featureGraphic.src = "./images/Taskwork_Graphic.png";
+    }
+
     const todoImg = document.getElementById('todoImg');
     if (todoImg.src.match("./images/ToDo_Today.png")) {
         todoImg.src = "./images/Dark_ToDoToday.png";
@@ -51,12 +59,28 @@ function toggleImages() {
         todoImg.src = "./images/ToDo_Today.png";
     }
 
+    const todoGraphic = document.getElementById('todoGraphic');
+    if (todoGraphic.src.match("./images/ToDoToday_Graphic")) {
+        todoGraphic.src = "./images/Dark_ToDoToday_Graphic.png";
+    }
+    else {
+        todoGraphic.src = "./images/ToDoToday_Graphic.png";
+    }
+
     const seanImg = document.getElementById('seanImg');
     if (seanImg.src.match("./images/Sean_vs_Ai.png")) {
         seanImg.src = "./images/Dark_Sean.png";
     }
     else {
         seanImg.src = "./images/Sean_vs_Ai.png.png";
+    }
+
+    const seanGraphic = document.getElementById('seanGraphic');
+    if (seanGraphic.src.match("./images/SeanvsAI_Graphic.png")) {
+        seanGraphic.src = "./images/Dark_SeanvsAI_Graphic.png";
+    }
+    else {
+        seanGraphic.src = "./images/SeanvsAI_Graphic.png";
     }
 
     // Contact Page Images
