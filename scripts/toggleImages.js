b6b401ee3d0bd90d6d7204afae1dee3132811348
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-26 11:01:29 -0400

    Completed dark mode graphics, created dynamic button to toggle modes.

--- scripts/toggleImages.js
+++ scripts/toggleImages.js
@@ -1,6 +1,18 @@
 'use strict';
 
 function toggleImages() {
+    // Change text on button when images change
+    const btn = document.getElementById("modeButton");
+
+    if (btn.value == "Light Mode") {
+        btn.value = "Dark Mode";
+        btn.innerHTML = "Dark Mode";
+    }
+    else {
+        btn.value = "Light Mode";
+        btn.innerHTML = "Light Mode";
+    }
+
     // About Page Images
     const hero = document.getElementById('heroImage');
     if (hero.src.match("./images/Hero_Body.png")) {
@@ -32,6 +44,78 @@ function toggleImages() {
     }
     else {
         featureImg.src = "./images/Featured_Project_Graphic.png";
+    }
+
+    const bulmaImg = document.getElementById('bulmaImg');
+    if (bulmaImg.src.match("./images/Bulma.png")) {
+        bulmaImg.src = "./images/Dark_Bulma.png";
+    }
+    else {
+        bulmaImg.src = "./images/Bulma.png";
+    }
+
+    const typescript = document.getElementById('typescriptImg');
+    if (typescript.src.match("./images/Typescript.png")) {
+        typescript.src = "./images/Dark_Typescript.png";
+    }
+    else {
+        typescript.src = "./images/Typescript.png";
+    }
+
+    const postgres = document.getElementById('postgresImg');
+    if (postgres.src.match("./images/Postgres.png")) {
+        postgres.src = "./images/Dark_Postgres.png";
+    }
+    else {
+        postgres.src = "./images/Postgres.png";
+    }
+
+    const canva = document.getElementById('canvaImg');
+    if (canva.src.match("./images/Canva.png")) {
+        canva.src = "./images/Dark_Canva.png";
+    }
+    else {
+        canva.src = "./images/Canva.png";
+    }
+
+    const adobe = document.getElementById('adobeImg');
+    if (adobe.src.match("./images/Adobe.png")) {
+        adobe.src = "./images/Dark_Adobe.png";
+    }
+    else {
+        adobe.src = "./images/Adobe.png";
+    }
+
+    const asana = document.getElementById('asanaImg');
+    if (asana.src.match("./images/Asana.png")) {
+        asana.src = "./images/Dark_Asana.png";
+    }
+    else {
+        asana.src = "./images/Asana.png";
+    }
+
+    const basecamp = document.getElementById('basecamp');
+    if (basecamp.src.match("./images/Basecamp.png")) {
+        basecamp.src = "./images/Dark_Basecamp.png";
+    }
+    else {
+        basecamp.src = "./images/Basecamp.png";
+    }
+
+    const aws = document.getElementById('awsImg');
+    if (aws.src.match("./images/AWS.png")) {
+        aws.src = "./images/Dark_Aws.png";
+    }
+    else {
+        aws.src = "./images/AWS.png";
+    }
+
+    const postico = document.getElementById('posticoImg');
+    if (postico.src.match("./images/Postico.png")) {
+        postico.src = "./images/Dark_Postico.png";
+    }
+    else {
+        postico.src = "./images/Postico.png";
     }
 
     // Portfolio Page Images
